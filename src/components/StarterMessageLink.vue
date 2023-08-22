3fd862a3ff4db4a4a2c5813e2101f539dc2b0e9c
Curt Grimes <curt@curtgrimes.com>
2023-08-22 16:05:29 -0500

    Move long class list to scoped styles

--- src/components/StarterMessageLink.vue
+++ src/components/StarterMessageLink.vue
@@ -9,10 +9,15 @@ defineProps<{
 <template>
   <component
     :is="url ? 'a' : 'button'" v-bind="{ ...(url ? { href: url, target: '_blank' } : {}) }"
-    class="inline-flex items-center justify-center gap-1 rounded-md border border-transparent p-2 text-blue-700 hover:border-blue-300 hover:bg-blue-100 md:justify-start"
   >
     <i-mdi-github v-if="icon === 'github'" />
     <i-mdi-lightning-bolt v-else-if="icon === 'stackblitz'" />
     {{ text }}
   </component>
 </template>
+
+<style scoped>
+a,button {
+  @apply inline-flex md:justify-start items-center justify-center gap-1 rounded-md border border-transparent px-2 py-1 text-blue-700 hover:border-blue-300 hover:bg-blue-100 ;
+}
+</style>
