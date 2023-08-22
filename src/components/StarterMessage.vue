a7de14b7966e6a53224afa5ea0fed256974f49ff
Curt Grimes <curt@curtgrimes.com>
2023-08-22 07:58:11 -0500

    Improve responsiveness

--- src/components/StarterMessage.vue
+++ src/components/StarterMessage.vue
@@ -11,8 +11,9 @@ const count = ref(0);
     class="bg-gray-200 max-w-xl mx-auto mt-12 p-5 rounded text-center flex gap-4 flex-col"
   >
     <h1 class="text-xl">Curt's Vite + Vue3 Starter</h1>
+    <p>{{ message }}</p>
 
-    <p class="flex gap-2 justify-center">
+    <p class="flex gap-2 justify-center flex-col md:flex-row">
       <StarterMessageLink
         url="https://github.com/curtgrimes/curt-vite-vue3-starter"
         text="GitHub"
@@ -28,6 +29,5 @@ const count = ref(0);
         :text="`Test reactivity: ${count}`"
       />
     </p>
-    <p>{{ message }}</p>
   </div>
 </template>
--- src/components/StarterMessageLink.vue
+++ src/components/StarterMessageLink.vue
@@ -10,7 +10,7 @@ defineProps<{
   <component
     :is="url ? 'a' : 'button'"
     v-bind="{ ...(url ? { href: url, target: '_blank' } : {}) }"
-    class="inline-flex items-center gap-1 text-blue-700 hover:bg-blue-100 border border-transparent hover:border-blue-300 p-2 rounded-md"
+    class="inline-flex items-center gap-1 justify-center md:justify-start text-blue-700 hover:bg-blue-100 border border-transparent hover:border-blue-300 p-2 rounded-md"
   >
     <i-mdi-github v-if="icon === 'github'" />
     <i-mdi-lightning-bolt v-else-if="icon === 'stackblitz'" />
